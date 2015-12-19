35ecdf18f4c319398dc9361a355cbd97b85f5fe5
Ziyang Li <ziyang.li.nk@gmail.com>
2015-12-19 22:47:19 +0100

    update

--- train_lstm_bitmap.sh
+++ train_lstm_bitmap.sh
@@ -6,7 +6,7 @@
 #BSUB -eo /home/kurse/oe14ireg/TextSegmentation/experience/stderr.txt
 #BSUB -oo /home/kurse/oe14ireg/TextSegmentation/experience/stdout.txt
 #BSUB -n 1
-#BSUB -M 20480
+#BSUB -M 30480
 #BSUB -W 600
 #BSUB -x
 #BSUB -q kurs3
