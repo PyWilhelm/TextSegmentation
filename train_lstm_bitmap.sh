d144af95bff2b81c3a6b7cbfb3a9c4b87c665421
Ziyang Li <ziyang.li.nk@gmail.com>
2015-12-19 22:22:28 +0100

    update

--- train_lstm_bitmap.sh
+++ train_lstm_bitmap.sh
@@ -4,10 +4,11 @@
 #BSUB -eo /home/kurse/oe14ireg/TextSegmentation/experience/stderr.txt
 #BSUB -oo /home/kurse/oe14ireg/TextSegmentation/experience/stdout.txt
 #BSUB -n 1
-#BSUB -M 10000
-#BSUB -W 60
+#BSUB -M 40960
+#BSUB -W 600
 #BSUB -x
-#BSUB -q kurs3
+#BSUB -q
+#BSUB -R "select[ nvd && avx2 ]"
 
 cd /home/kurse/oe14ireg/TextSegmentation/experience/
 THEANO_FLAGS='mode=FAST_RUN,device=gpu,force_device=True,floatX=float32' python3 /home/kurse/oe14ireg/TextSegmentation/experience/LSTM_bitmap.py training_x training_y LSTM_bitmap.model
